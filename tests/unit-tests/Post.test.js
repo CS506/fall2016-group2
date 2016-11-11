884534d22f73a89cae3d5132fbfa93f4bb0c67d1
Angus Dobson <asdobson@iu.edu>
2016-11-11 03:11:42 -0500

    Initial post model test

---
+++ tests/unit-tests/Post.test.js
@@ -0,0 +1,62 @@
+var async = require('async')
+  , assert = require('chai').assert
+  , should = require('chai').should()
+  , blueprint = require('@onehilltech/blueprint')
+  , appPath = require('../fixtures/appPath')
+  , it = require("mocha").it
+  , before = require("mocha").before
+  , describe = require("mocha").describe
+  , after = require("mocha").after
+  , Post = require('../../app/models/Post')
+  , User = require('../../app/models/User')
+  , posts = require('../fixtures/posts')
+  , winston = require('winston');
+;
+
+describe('Post Model Test', function () {
+    var postuser = {
+        "username": "asdobson",
+        "password": "test1234"
+    };
+
+    before(function (done) {
+        async.waterfall([
+            function (callback) { 
+                blueprint.testing.createApplicationAndStart(appPath, callback)
+            }, 
+            function (app, callback) {
+                User.create(postuser, function (err, newuser) {
+                    for (let item of posts) {
+                        item.createdBy = newuser.id;
+                        Post.create(item);
+                    }
+                    callback(null);
+                });
+            }
+        ], done);
+    });
+
+    it('Should count all posts with #wonderful tag', function (done) {
+        Post.count({ tags: 'wonderful' }, function (err, count) {
+            count.should.equal(3);
+            done();
+        })    
+    });
+
+    it('Should find the username for the first post with "test" tag', function (done) {
+        Post
+        .findOne({ tags: 'test' })
+        .populate("createdBy")
+        .exec(function (err, found) {
+            found.createdBy.username.should.equal("asdobson");
+            done();
+        });
+    });
+
+    after(function (done) {
+        Post.remove().exec();
+        User.remove().exec();
+        done();
+    });
+
+});
